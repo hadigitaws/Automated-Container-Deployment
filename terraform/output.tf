335a215f666e2f8e1c94ad2406f89aeb5d4f79f1
Hadi <syedhadi708@gmail.com>
2026-03-03 21:53:00 +0000

    Initial clean commit

---
+++ terraform/output.tf
@@ -0,0 +1,9 @@
+output "instance_public_ip" {
+  description = "Public IP of EC2"
+  value       = aws_instance.web_server.public_ip
+}
+
+output "instance_id" {
+  description = "Instance ID"
+  value       = aws_instance.web_server.id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.micro"
+}
+
+variable "public_key_path" {
+  description = "Path to SSH public key"
+  default     = "/home/hadi/.ssh/id_rsa.pub"
+}
